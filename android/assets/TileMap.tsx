9e4f96701d49b365fdf213d875980974c79367c6
Mario <57414832+MaRio0607@users.noreply.github.com>
2021-05-16 13:09:56 -0500

    tileset direcion

--- android/assets/TileMap.tsx
+++ android/assets/TileMap.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="TileMap" tilewidth="16" tileheight="16" tilecount="198" columns="18">
- <image source="D:/NEW RESPALDO/Games/Adventurer/Visuals/Levels/stringstar fields/tileset.png" width="288" height="176"/>
+ <image source="tileset.png"/>
 </tileset>
